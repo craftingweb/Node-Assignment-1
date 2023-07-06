779ed62079d78723da7480a8d3e699626bf6cf62
Vitali <vitaliy.prymak@cix.csi.cuny.edu>
2023-07-06 16:51:44 -0400

    error handling

--- server.js
+++ server.js
@@ -19,38 +19,79 @@ app.get("/todos", (req, res) => {
 });
 
 // Get a specific todo
-app.get("/todos/:id", (req, res) => {
-  console.log(typeof todos);
-  const todoId = parseInt(req.params.id, 10);
-  const todo = todos.find((todo) => todo.id === todoId);
-  res.send(todo);
-});
+// app.get("/todos/:id", (req, res) => {
+//   console.log(typeof todos);
+//   const todoId = parseInt(req.params.id, 10);
+//   const todo = todos.find((todo) => todo.id === todoId);
+//   res.send(todo);
+// });
 
 // Create a new todo
+// app.post("/todos", (req, res) => {
+//   const newTodo = req.body;
+//   console.log("newTodo", newTodo);
+//   todos.push(newTodo);
+//   res.send(newTodo);
+// });
+
 app.post("/todos", (req, res) => {
   const newTodo = req.body;
-  console.log("newTodo", newTodo);
   todos.push(newTodo);
-  res.send(newTodo);
+  res.status(201).send(newTodo);
 });
 
 // update todo
+// app.patch("/todos/:id", (req, res) => {
+//   const todoId = parseInt(req.params.id, 10);
+//   const todoUpdates = req.body;
+//   const todoIndex = todos.findIndex((todo) => todo.id === todoId);
+//   const updatedTodo = { ...todos[todoIndex], ...todoUpdates };
+//   todos[todoIndex] = updatedTodo;
+//   // console.log("updatedtodo", updatedtodo);
+//   res.send(updatedTodo);
+// });
+
+// Delete a specific todo
+// app.delete("/todos/:id", (req, res) => {
+//   const todoId = parseInt(req.params.id, 10);
+//   const todoIndex = todos.findIndex((todo) => todo.id === todoId);
+//   todos.splice(todoIndex, 1);
+//   res.send({ message: "Todo deleted successfully" });
+// });
+
+// Get a specific todo
+app.get("/todos/:id", (req, res) => {
+  const todoId = parseInt(req.params.id);
+  const todo = todos.find((todo) => todo.id === todoId);
+  if (todo) {
+    res.send(todo);
+  } else {
+    res.status(404).send({ message: "Todo not found" });
+  }
+});
+
 app.patch("/todos/:id", (req, res) => {
   const todoId = parseInt(req.params.id, 10);
   const todoUpdates = req.body;
   const todoIndex = todos.findIndex((todo) => todo.id === todoId);
   const updatedTodo = { ...todos[todoIndex], ...todoUpdates };
-  todos[todoIndex] = updatedTodo;
-  // console.log("updatedJob", updatedJob);
-  res.send(updatedTodo);
+  if (todoIndex !== -1) {
+    todos[todoIndex] = updatedTodo;
+    res.send(updatedTodo);
+  } else {
+    res.status(404).send({ message: "Todo not found" });
+  }
 });
 
-// Delete a specific todo
 app.delete("/todos/:id", (req, res) => {
   const todoId = parseInt(req.params.id, 10);
   const todoIndex = todos.findIndex((todo) => todo.id === todoId);
-  todos.splice(todoIndex, 1);
-  res.send({ message: "Todo deleted successfully" });
+  if (todoIndex !== -1) {
+    todos.splice(todoIndex, 1);
+    res.status(204).send();
+  } else {
+    res.status(404).send({ message: "Todo not found" });
+  }
 });
 
 app.get("/", (req, res) => {
